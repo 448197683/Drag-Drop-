50612202d187de4eba459b4b199b15c1d0e693c0
448197683 <448197683@qq.com>
2021-11-18 14:41:27 +0800

    finish

--- 02 Draggable/index.js
+++ 02 Draggable/index.js
@@ -7,11 +7,11 @@ items.forEach((item) => {
   item.addEventListener('dragenter', (e) => {
     selecter = e.target;
     selecter.style.background = `blue`;
-    selecter.style.border = `dotted`;
+    selecter.style.border = `3px dotted black`;
   });
   item.addEventListener('dragleave', (e) => {
     selecter.style.background = `white`;
-    selecter.style.border = `5px solid`;
+    selecter.style.border = `1px solid black`;
   });
   img.addEventListener('dragend', (e) => {
     if (selecter.className !== 'item') {
